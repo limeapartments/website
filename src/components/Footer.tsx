8044a16cc536dbe61a0433e828b0e8d7b7a945b8
Chance Hudson <jchancehud@gmail.com>
2019-03-02 05:41:47 -0600

    Update footer formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
+import MouseoverA from './MouseoverA';
 
 const PulseKeyframes = keyframes`
   0% {
@@ -26,19 +27,47 @@ const Background = styled.div`
   align-items: center;
   justify-content: space-around;
   animation: ${PulseKeyframes} 40s ease-in 0s infinite alternate;
+  padding: 20px;
 `;
 
 export default class Footer extends React.Component {
   render() {
     return (
       <Background>
+        <div
+          style={{ color: 'white', cursor: 'pointer', fontSize: 16 }}
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+          }}
+        >
+          BACK TO TOP
+        </div>
+        <div style={{ height: 2, width: 300, backgroundColor: 'rgba(191, 55, 145, 1)'}} />
         <img style={{ height: 40 }} src={require('../../static/images/lime-logo.png')} />
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <div style={{ fontFamily: 'Open Sans', fontSize: 13, color: 'white' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ fontFamily: 'Open Sans, Muli', fontSize: 14, color: 'white', textAlign: 'center' }}>
             Lime Apartments | 2904 Lyndale Avenue South • Minneapolis, Minnesota 55408 | 855.246.1190 | refresh@limeapartments.com
           </div>
-          <div style={{ fontFamily: 'Open Sans', fontSize: 13, color: 'white' }}>
-            ©2018 Saturday Properties | Photography & Site Design: Carbon Creative
+          <div style={{ fontFamily: 'Open Sans, Muli', fontSize: 14, color: 'white', textAlign: 'center', marginTop: 10 }}>
+            ©2018
+            <MouseoverA
+              href="http://www.saturdayproperties.com/"
+              target="_blank"
+              activeStyle={{ color: 'black' }}
+              inactiveStyle={{ color: 'white', textDecoration: 'none' }}
+              style={{ marginLeft: 4 }}
+            >
+              Saturday Properties
+            </MouseoverA> | Photography & Site Design:
+            <MouseoverA
+              href="http://www.carboncreative.com/"
+              target="_blank"
+              activeStyle={{ color: 'black' }}
+              inactiveStyle={{ color: 'white', textDecoration: 'none' }}
+              style={{ marginLeft: 4 }}
+            >
+              Carbon Creative
+            </MouseoverA>
           </div>
         </div>
       </Background>
---
+++ src/components/MouseoverA.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class MouseoverA extends React.Component<{
+  href: string,
+  target?: string,
+  activeStyle?: any,
+  inactiveStyle?: any,
+  style?: any
+}> {
+  state = {
+    active: false,
+  };
+  render() {
+    return (
+      <a
+        onMouseEnter={() => this.setState({ active: true })}
+        onMouseLeave={() => this.setState({ active: false })}
+        style={{
+          ...(this.state.active ? this.props.activeStyle : this.props.inactiveStyle),
+          ...(this.props.style || {})
+        }}
+        href={this.props.href}
+        target={this.props.target}
+      >
+        {this.props.children}
+      </a>
+    );
+  }
+}
