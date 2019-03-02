f9e7fb227ff45ed9cb9a03c4a1f14486a199e4d4
Chance Hudson <jchancehud@gmail.com>
2019-03-02 05:54:02 -0600

    Add key to looped div

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -9,9 +9,9 @@ export default class ImageGrid extends React.Component<{
   render() {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-        {this.props.data.map(data => {
+        {this.props.data.map((data, index) => {
           return (
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 10 }}>
+            <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 10 }}>
               <img src={data.image} />
               <span style={{ fontFamily: 'Sanchez', fontSize: 20, color: 'white' }}>{data.text}</span>
             </div>
