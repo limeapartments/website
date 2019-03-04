9aec6028de4d7e9c659dff352c143ee17284006d
Chance Hudson <jchancehud@gmail.com>
2019-03-04 11:00:14 -0600

    Add images to gallery page

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -3,12 +3,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import BlackStriped from './components/BlackStriped';
 import { VFlex } from './components/Shared';
+import ImageGrid from './components/ImageGrid';
 
 export default () => (
   <>
     <Header />
     <BlackStriped>
       <VFlex>
+        <ImageGrid data={Array(18).fill(0).map((_, index) => {
+          return {
+            image: require(`../static/images/gallery-grid${index+1}.jpg`),
+          };
+        })} />
       </VFlex>
     </BlackStriped>
     <Footer />
--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ImageGrid extends React.Component<{
   data: {
     image: any,
-    text: string,
+    text?: string,
   }[]
 }> {
   render() {
