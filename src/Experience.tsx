6e00c99d5e579d6774cb57f5b9542851f38bce5f
Chance Hudson <jchancehud@gmail.com>
2019-03-05 00:04:32 -0600

    Add video to experience component

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -5,6 +5,17 @@ import Footer from './components/Footer';
 export default () => (
   <>
     <Header />
+      <div style={{ textAlign: 'center', padding: 20 }}>
+        <iframe
+          src="https://player.vimeo.com/video/122110529?color=ffffff&title=0&byline=0&portrait=0"
+          width="1165"
+          height="498"
+          frameborder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen
+        />
+      </div>
     <Footer />
   </>
 );
