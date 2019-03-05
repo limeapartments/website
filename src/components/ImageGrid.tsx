2f619eb4a1d48b52a56b816f751b62a675f9e48f
Chance Hudson <jchancehud@gmail.com>
2019-03-05 00:41:46 -0600

    Add links to ImageGrid

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,21 +1,46 @@
 import React from 'react';
 
+interface ImageData {
+  image: any,
+  text?: string,
+  link?: string
+}
+
 export default class ImageGrid extends React.Component<{
-  data: {
-    image: any,
-    text?: string,
-  }[]
+  data: ImageData[],
+  style?: any
 }> {
+  renderImage = (data: ImageData, index: number) => {
+    return (
+      <div
+        key={index}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: 10,
+          ...( data.link ? { cursor: 'pointer' } : {} )
+        }}
+        onClick={() => data.link && window.open(data.link, '_blank')}
+      >
+        <img src={data.image} />
+        <span style={{ fontFamily: 'Sanchez', fontSize: 20, color: 'white' }}>
+          {data.text}
+        </span>
+      </div>
+    );
+  };
+
   render() {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        ...(this.props.style || {})
+      }}>
         {this.props.data.map((data, index) => {
-          return (
-            <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 10 }}>
-              <img src={data.image} />
-              <span style={{ fontFamily: 'Sanchez', fontSize: 20, color: 'white' }}>{data.text}</span>
-            </div>
-          );
+          return this.renderImage(data, index);
         })}
       </div>
     );
