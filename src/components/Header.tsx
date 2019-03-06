5d0302e050460217761bc469da56595ae576b913
Chance Hudson <jchancehud@gmail.com>
2019-03-05 22:46:54 -0600

    Header layout fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,85 +35,83 @@ class Header extends React.Component<Props> {
 
   render() {
     return (
-      <div style={{ height: 500, width: '100%', }}>
-        <Carousel images={this.props.images}>
-          <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
-            <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
-              <div>
-                <a href="https://limeapartments.com">
-                  <img src={require('../../static/images/lime-logo.png')} />
-                </a>
-                <TitleDiv style={{ paddingTop: 20 }}>
-                  REFRESHING APARTMENTS
-                </TitleDiv>
-              </div>
-              <a href="https://www.facebook.com/pages/Lime-Apartments/389529864487875?skip_nax_wizard=true">
-                <img src={require('../../static/images/facebook-icon.png')} />
+      <Carousel images={this.props.images}>
+        <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
+          <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
+            <div>
+              <a href="https://limeapartments.com">
+                <img style={{ marginTop: 60 }} src={require('../../static/images/lime-logo.png')} />
               </a>
+              <TitleDiv style={{ marginBottom: 50, paddingTop: 20 }}>
+                REFRESHING APARTMENTS
+              </TitleDiv>
             </div>
-            <div style={{
-              height: 60,
-              width: '100%',
-              backgroundColor: 'rgba(219, 63, 168, 0.7)',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-around',
-              paddingHorizontal: 200,
-              flexWrap: 'wrap'
-            }}>
-              <MouseoverLink
-                to="/amenities"
-                { ...this.colorsForPath('/amenities')}
-              >
-                Amenities
-              </MouseoverLink>
-              <MouseoverLink
-                to="/experience"
-                { ...this.colorsForPath('/experience')}
-              >
-                Experience
-              </MouseoverLink>
-              <MouseoverLink
-                to="/location"
-                { ...this.colorsForPath('/location')}
-              >
-                Location
-              </MouseoverLink>
-              <MouseoverLink
-                to="/floor-plans"
-                { ...this.colorsForPath('/floor-plans')}
-              >
-                Floor Plans
-              </MouseoverLink>
-              <MouseoverLink
-                to="/gallery"
-                { ...this.colorsForPath('/gallery')}
-              >
-                Gallery
-              </MouseoverLink>
-              <MouseoverLink
-                to="/residents"
-                { ...this.colorsForPath('/residents')}
-              >
-                Residents
-              </MouseoverLink>
-              <MouseoverLink
-                to="/contact"
-                { ...this.colorsForPath('/contact')}
-              >
-                Contact
-              </MouseoverLink>
-              <MouseoverLink
-                to="/redirect?url=https%3A%2F%2Fwww.rhris.com%2Fapplynow.cfm%3FsiteID%3D26756A"
-                activeColor="black"
-                inactiveColor="white"
-              >
-                Apply Now
-              </MouseoverLink>
-            </div>
+            <a style={{ marginTop: 8, marginBottom: 8 }} href="https://www.facebook.com/pages/Lime-Apartments/389529864487875?skip_nax_wizard=true">
+              <img src={require('../../static/images/facebook-icon.png')} />
+            </a>
           </div>
-        </Carousel>
-      </div>
+          <div style={{
+            width: '100%',
+            backgroundColor: 'rgba(219, 63, 168, 0.7)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            flexWrap: 'wrap',
+            paddingTop: 8,
+            paddingBottom: 8
+          }}>
+            <MouseoverLink
+              to="/amenities"
+              { ...this.colorsForPath('/amenities')}
+            >
+              Amenities
+            </MouseoverLink>
+            <MouseoverLink
+              to="/experience"
+              { ...this.colorsForPath('/experience')}
+            >
+              Experience
+            </MouseoverLink>
+            <MouseoverLink
+              to="/location"
+              { ...this.colorsForPath('/location')}
+            >
+              Location
+            </MouseoverLink>
+            <MouseoverLink
+              to="/floor-plans"
+              { ...this.colorsForPath('/floor-plans')}
+            >
+              Floor Plans
+            </MouseoverLink>
+            <MouseoverLink
+              to="/gallery"
+              { ...this.colorsForPath('/gallery')}
+            >
+              Gallery
+            </MouseoverLink>
+            <MouseoverLink
+              to="/residents"
+              { ...this.colorsForPath('/residents')}
+            >
+              Residents
+            </MouseoverLink>
+            <MouseoverLink
+              to="/contact"
+              { ...this.colorsForPath('/contact')}
+            >
+              Contact
+            </MouseoverLink>
+            <MouseoverLink
+              to="/redirect?url=https%3A%2F%2Fwww.rhris.com%2Fapplynow.cfm%3FsiteID%3D26756A"
+              activeColor="black"
+              inactiveColor="white"
+            >
+              Apply Now
+            </MouseoverLink>
+          </div>
+        </div>
+      </Carousel>
     );
   }
 }
