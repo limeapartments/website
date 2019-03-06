9931f9a54f18a610415b8124c5f757baee80430d
Chance Hudson <jchancehud@gmail.com>
2019-03-05 22:59:56 -0600

    Home page layout fixes

--- src/Home.tsx
+++ src/Home.tsx
@@ -85,7 +85,17 @@ export default () => (
         <SubtitleDiv>
           CONNECT WITH LIME ON FACEBOOK
         </SubtitleDiv>
-        <div className="fb-page" data-href="https://www.facebook.com/Lime-Apartments-389529864487875/" data-width="500" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true">
+        <div
+          style={{
+            maxWidth: 500
+          }}
+          className="fb-page"
+          data-href="https://www.facebook.com/Lime-Apartments-389529864487875/"
+          data-small-header="false"
+          data-adapt-container-width="false"
+          data-hide-cover="false"
+          data-show-facepile="true"
+        >
           <blockquote cite="https://www.facebook.com/Lime-Apartments-389529864487875/" className="fb-xfbml-parse-ignore">
             <a href="https://www.facebook.com/Lime-Apartments-389529864487875/">
               Lime Apartments
@@ -102,36 +112,48 @@ export default () => (
         <SubtitleDiv>
           DROP BY AND SAY HI...
         </SubtitleDiv>
-        <div style={{ display: 'flex' }}>
+        <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
           <iframe
-            style={{ border: 'none', marginRight: 12 }}
+            style={{
+              border: 'none',
+              marginLeft: 12,
+              marginRight: 12,
+              flex: 1,
+              maxWidth: 500,
+              height: 600
+            }}
             src="https://guestcard.ltsolutions.com/contactus.aspx?campaign=Lime005@myLTSMail.com&amp;csslink=https://gistcdn.githack.com/JChanceHud/17cb194ba9ae69814b6f5d5369bacf48/raw/024e7f82a9b07da68e6f5d3f31b6d1122de7685b/ContactUs.css&amp;comments=text"
-            width="100%"
-            height="600"
           />
           <iframe
-            style={{ border: 'none' }}
+            style={{
+              flex: 1,
+              border: 'none',
+              maxWidth: 500,
+              height: 450,
+              marginLeft: 12,
+              marginRight: 12
+            }}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.699373434938!2d-93.28862300000003!3d44.949779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62786279600b1%3A0x50632f6c436231c9!2sLime+Apartments!5e0!3m2!1sen!2sus!4v1402594082801"
-            width="600"
-            height="450"
           />
         </div>
       </VFlex>
     </BlackStriped>
     <SectionImageDiv image={require('../static/images/home-background-section2.jpg')}>
-      <VFlex>
-        <TitleDiv>
-          UPTOWN
-        </TitleDiv>
-        <SubtitleDiv>
-          THE LYN-LAKE LYFESTYLE
-        </SubtitleDiv>
-        <ProgressBar color="#00ffcc" percent={0.5} title="shopping" />
-        <ProgressBar color="#ff00ff" percent={0.63} title="arts & entertainment" />
-        <ProgressBar color="#fffa61" percent={0.75} title="bars & restaurants" />
-        <ProgressBar color="#ff612a" percent={0.87} title="lakes & bikes" />
-        <ProgressBar color="#ffffff" percent={1} title="awesome!" />
-      </VFlex>
+      <div style={{ width: '80%' }}>
+        <VFlex>
+          <TitleDiv>
+            UPTOWN
+          </TitleDiv>
+          <SubtitleDiv>
+            THE LYN-LAKE LYFESTYLE
+          </SubtitleDiv>
+          <ProgressBar color="#00ffcc" percent={0.5} title="shopping" />
+          <ProgressBar color="#ff00ff" percent={0.63} title="arts & entertainment" />
+          <ProgressBar color="#fffa61" percent={0.75} title="bars & restaurants" />
+          <ProgressBar color="#ff612a" percent={0.87} title="lakes & bikes" />
+          <ProgressBar color="#ffffff" percent={1} title="awesome!" />
+        </VFlex>
+      </div>
     </SectionImageDiv>
     <SectionImageDiv image={require('../static/images/home-background-section3.jpg')}>
       <VFlex>
