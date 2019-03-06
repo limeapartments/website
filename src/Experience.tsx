eeb9e84d3d38c008fc4af712de50d89736c71116
Chance Hudson <jchancehud@gmail.com>
2019-03-06 01:01:58 -0600

    Experience video padding fixes

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CallNow from './components/CallNow';
+import { VFlex } from './components/Shared';
 
 export default () => (
   <>
     <Header images={[
       require('../static/images/experience-header1.jpg'),
     ]} />
-    <div style={{ width: '90%', textAlign: 'center', padding: 20 }}>
-      <iframe
-        src="https://player.vimeo.com/video/122110529?color=ffffff&title=0&byline=0&portrait=0"
-        style={{
-          width: '70%',
-          maxWidth: 900,
-          height: 600
-        }}
-        frameBorder="0"
-      />
-    </div>
+    <VFlex>
+      <div style={{ width: '90%', textAlign: 'center', padding: 20 }}>
+        <iframe
+          src="https://player.vimeo.com/video/122110529?color=ffffff&title=0&byline=0&portrait=0"
+          style={{
+            width: '100%',
+            maxWidth: 900,
+            height: '40vw',
+            maxHeight: 400
+          }}
+          frameBorder="0"
+        />
+      </div>
+    </VFlex>
     <Footer />
     <CallNow />
   </>
