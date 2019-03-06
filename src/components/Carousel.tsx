e528a8d81ff126eced693f0c13b335875ee9346f
Chance Hudson <jchancehud@gmail.com>
2019-03-06 02:08:53 -0600

    Animated carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,50 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+/**
+ * An auto scrolling image carousel
+ **/
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const ImageDiv = styled.div<{
+  image: any,
+  index: number,
+  activeIndex: number,
+  nextIndex: number,
+  lastIndex: number
+}>`
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-position: bottom;
+  background-repeat: no-repeat;
+  z-index: ${props => -10 + (-1 * props.index)};
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: ${props => props.index === props.lastIndex ? 1 : 0};
+  animation: ${props => props.index === props.lastIndex ? fadeOut : (props.index === props.activeIndex ? fadeIn : '')};
+  animation-duration: 2s;
+  animation-delay: 3s;
+  animation-timing: linear;
+`;
 
 export default class Carousel extends React.Component<{
   images: any[]
@@ -9,10 +55,18 @@ export default class Carousel extends React.Component<{
 
   timer: any;
 
+  lastImageIndex = () => {
+    return ((this.state.imageIndex - 1) + this.props.images.length) % this.props.images.length;
+  }
+
+  nextImageIndex = () => {
+    return (this.state.imageIndex + 1) % this.props.images.length;
+  };
+
   componentDidMount() {
     this.timer = setInterval(() => {
       this.setState({
-        imageIndex: (this.state.imageIndex + 1) % this.props.images.length
+        imageIndex: this.nextImageIndex()
       });
     }, 5000);
   }
@@ -24,19 +78,23 @@ export default class Carousel extends React.Component<{
 
   render() {
     return (
-      <div style={{
-        backgroundImage: `url(${this.props.images[this.state.imageIndex]})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'bottom',
-        backgroundRepeat: 'no-repeat',
-        height: 'inherit',
-        width: '100%'
-      }}>
+      <div style={{ height: 'inherit', width: '100%' }}>
         <div style={{
+          position: 'relative',
           width: '100%',
           height: '100%',
           backgroundColor: 'rgba(0, 0, 0, 0.3)'
         }}>
+          {this.props.images.map((image: any, index: number) => (
+            <ImageDiv
+              key={index}
+              image={image}
+              index={index}
+              activeIndex={this.state.imageIndex}
+              nextIndex={this.nextImageIndex()}
+              lastIndex={this.lastImageIndex()}
+            />
+          ))}
           {this.props.children}
         </div>
       </div>
