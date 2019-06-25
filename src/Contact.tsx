ccc373aabc644314bdc81359a531ea3ea71766d2
limeapart <48071694+limeapart@users.noreply.github.com>
2019-06-25 10:05:04 -0700

    Update Contact.tsx

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -22,7 +22,15 @@ export default () => (
 	  <HFlex>
 		
 		<div class="contactform">
-		
+		<iframe
+            style={{
+              border: 'none',
+              width: '100%',
+              maxWidth: 600,
+              height: 250,
+            }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.699373434938!2d-93.28862300000003!3d44.949779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62786279600b1%3A0x50632f6c436231c9!2sLime+Apartments!5e0!3m2!1sen!2sus!4v1402594082801"
+          />
 		<div class="headertext">Contact Us</div>
 		
 		<form action="https://perfectwebsolutions.info/limecontactfromphp/index.php" method="get">
